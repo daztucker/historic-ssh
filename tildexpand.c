915312caf7d853bfc161c184030a26142d85665a
ylo <ylo@cs.hut.fi>
1995-07-13 01:41:03 +0000

    Revision 1.2  1995/07/13  01:41:03  ylo 	Removed "Last modified" header. 	Added cvs log.

--- tildexpand.c
+++ tildexpand.c
@@ -8,9 +8,18 @@ Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
                    All rights reserved
 
 Created: Wed Jul 12 01:07:36 1995 ylo
-Last modified: Wed Jul 12 01:28:31 1995 ylo
 
 */
+
+/*
+ * $Id: tildexpand.c,v 1.2 1995/07/13 01:41:03 ylo Exp $
+ * $Log: tildexpand.c,v $
+ * Revision 1.2  1995/07/13  01:41:03  ylo
+ * 	Removed "Last modified" header.
+ * 	Added cvs log.
+ *
+ * $Endlog$
+ */
 
 #include "includes.h"
 #include "xmalloc.h"

fa78ab395ade7c964f7fc83ae41dd894ab0cd7d3
ylo <ylo@cs.hut.fi>
1995-07-13 01:40:55 +0000

    Revision 1.2  1995/07/13  01:40:55  ylo 	Removed "Last modified" header. 	Added cvs log.

--- strerror.c
+++ strerror.c
@@ -8,11 +8,20 @@ Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
                    All rights reserved
 
 Created: Wed Mar 22 18:18:21 1995 ylo
-Last modified: Wed Apr 26 11:38:47 1995 ylo
 
 Replacement for strerror for systems that don't have it.
 
 */
+
+/*
+ * $Id: strerror.c,v 1.2 1995/07/13 01:40:55 ylo Exp $
+ * $Log: strerror.c,v $
+ * Revision 1.2  1995/07/13  01:40:55  ylo
+ * 	Removed "Last modified" header.
+ * 	Added cvs log.
+ *
+ * $Endlog$
+ */
 
 
 #include <stdio.h>

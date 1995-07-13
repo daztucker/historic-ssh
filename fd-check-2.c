541c61e74d6d94973a3439136638a8592cf60634
ylo <ylo@cs.hut.fi>
1995-07-13 01:23:26 +0000

    Revision 1.2  1995/07/13  01:23:26  ylo 	Removed "Last modified" header. 	Added cvs log.

--- fd-check-2.c
+++ fd-check-2.c
@@ -8,11 +8,20 @@ Copyright (c) 1995 Tatu Ylonen <ylo@cs.hut.fi>, Espoo, Finland
                    All rights reserved
 
 Created: Sun Apr 23 19:37:57 1995 ylo
-Last modified: Sun Apr 23 19:53:37 1995 ylo
 
 Prints "yes" if file descriptor 25 is valid, "no" otherwise.
 
 */
+
+/*
+ * $Id: fd-check-2.c,v 1.2 1995/07/13 01:23:26 ylo Exp $
+ * $Log: fd-check-2.c,v $
+ * Revision 1.2  1995/07/13  01:23:26  ylo
+ * 	Removed "Last modified" header.
+ * 	Added cvs log.
+ *
+ * $Endlog$
+ */
 
 #include <stdio.h>
 #include <fcntl.h>

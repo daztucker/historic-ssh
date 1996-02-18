82329ad5a4203378d43b8efd6e49afc5f301a182
ylo <ylo@cs.hut.fi>
1996-02-18 21:38:11 +0000

    Revision 1.1.1.1  1996/02/18 21:38:11  ylo 	Imported ssh-1.2.13.

--- des.c
+++ des.c
@@ -25,8 +25,11 @@ cryptography and data security, including the following:
 */
 
 /*
- * $Id: des.c,v 1.2 1995/07/13 01:22:25 ylo Exp $
+ * $Id: des.c,v 1.1.1.1 1996/02/18 21:38:11 ylo Exp $
  * $Log: des.c,v $
+ * Revision 1.1.1.1  1996/02/18 21:38:11  ylo
+ * 	Imported ssh-1.2.13.
+ *
  * Revision 1.2  1995/07/13  01:22:25  ylo
  * 	Added cvs log.
  *
